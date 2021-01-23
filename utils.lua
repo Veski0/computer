b5f8ed6ece443002bed2da711f3c7e1d10757fce
Veski0 <holocene@null.net>
2021-01-23 06:30:52 +0000

    Inital Commit. Moving from Pastebin to Github.

---
+++ utils.lua
@@ -0,0 +1,41 @@
+-- Constants                         --
+-- Valid Modem Directions
+modem_directions =
+ "up", "down",
+ "left", "right",
+ "bottom", "top"
+}
+---------------------------------------
+
+-- Function: contains
+-- Purpose : array contains item?
+-- Args    : {Table, Element}
+-- Returns : {Boolean}
+contains = function (tab, val)
+  for index, value in ipairs(tab) do
+    if value == value then
+      return true
+    end
+  end
+  return false
+end
+---------------------------------------
+
+-- Function: table_length
+-- Purpose : calculate length of table.
+-- Args    : {Table}
+-- Returns : {Number}
+table_length = function (t)
+  local count = 0
+  for _ in pairs(t) do
+    count = count + 1
+  end
+  return count
+end
+---------------------------------------
+
+return {
+  modem_directions=modem_directions,
+  contains=contains,
+  table_length=table_length
+}
